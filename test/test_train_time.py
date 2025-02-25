728a7dfa336a6cc2e4694567b4a94730dcd3729e
tasnimkh24 <tasnim.khelil@esprit.tn>
2025-02-25 18:22:56 +0100

    Fixing train model1

--- test/test_train_time.py
+++ test/test_train_time.py
@@ -1,9 +1,13 @@
 import time
+import mlflow
 from src.model_pipeline import prepare_data, train_model
 
 def test_training_time():
     X_train, _, y_train, _, _, _ = prepare_data("data/train.csv", "data/test.csv")
-    start_time = time.time()
-    train_model(X_train, y_train)
-    training_time = time.time() - start_time
-    assert training_time < 60  # Vérifiez que l'entraînement prend moins de 60 secondes
+    
+    # Start an MLflow run for this test
+    with mlflow.start_run():
+        start_time = time.time()
+        train_model(X_train, y_train)
+        training_time = time.time() - start_time
+        assert training_time < 10, "Training took too long!"
